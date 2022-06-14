fb519723ea9477f699c8f985c94a5f02418a813d
unknown <abegundeosamuel@gmail.com>
2022-06-14 08:32:50 +0100

    Loader

---
+++ client/src/Pages/Loader/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Loader() {
+  return (
+    <div className="loader-container">
+      <div className='loader'>
+        <span className="let1">l</span>
+        <span className="let2">i</span>
+        <span className="let3">g</span>
+        <span className="let4">h</span>
+        <span className="let5">t</span>
+        <span className="let4">s</span>
+        <span className="let2">_</span>
+        <span className="let6">U</span>
+        <span className="let7">P</span>
+      </div>
+    </div>
+  )
+}
