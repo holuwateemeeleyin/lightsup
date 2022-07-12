76e487aeb9c3ec9790c1823900d22018086f9151
unknown <abegundeosamuel@gmail.com>
2022-07-12 12:01:55 +0100

    Eth page

--- client/src/Pages/Eth/index.js
+++ client/src/Pages/Eth/index.js
@@ -1,18 +1,21 @@
 import React from 'react'
-import SolanaHeader from '../Header/Solana/SolanaHeader'
-export default function Eth() {
+import EthHeader from '../Header/Eth/EthHeader'
+import Card from '../../Component/Eth/Card'
+import Table from '../../Component/Eth/Table'
+export default function Eth({eth}) {
+
   return (
     <div className='solana-parent-container'>
-      <SolanaHeader/>
+      <EthHeader/>
       <div className='solana'>
         <div className='solana-header'>
           Solana Trending NFT
         </div>
-        {/* <Card cards={solana}/> */}
+        <Card cards={eth}/>
         <div className='solana-table-header'>
           Upcomming Projects
         </div>
-        {/* <Table table={solana}/> */}
+        <Table table={eth}/>
       </div>
     </div>
   )
